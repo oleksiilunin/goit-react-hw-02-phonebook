a8389850605fc86354853836ef8770f579841abd
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-22 15:44:06 +0300

    Create component Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { HeaderTitle } from './Header.styled';
+
+const Header = ({ headerTitle }) => {
+  return <HeaderTitle>{headerTitle}</HeaderTitle>;
+};
+
+export { Header };
