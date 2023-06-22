4102a1e38898eab6f4b3101ada2decb41010f402
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-22 15:45:22 +0300

    Create component Section

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+import { SectionTitle } from './Section.styled';
+import { SectionWrapper } from './Section.styled';
+
+const Section = ({ title, children }) => (
+  <SectionWrapper>
+    {title && <SectionTitle>{title}</SectionTitle>}
+    {children}
+  </SectionWrapper>
+);
+
+Section.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.any.isRequired,
+};
+
+export { Section };
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const SectionWrapper = styled.section`
+  margin-bottom: 24px;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+`;
+
+const SectionTitle = styled.h2`
+  margin-bottom: 12px;
+`;
+
+export { SectionWrapper };
+export { SectionTitle };
