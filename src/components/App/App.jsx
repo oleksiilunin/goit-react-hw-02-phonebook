012914cf8a60d411e52d16138f949a31521d8ece
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-25 19:19:32 +0300

    Move contacts in separate file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,7 @@ import { ContactForm } from 'components/ContactForm';
 import { Filter } from 'components/Filter';
 import { ContactsList } from 'components/ContactsList';
 import { GlobalStyles } from 'components/GlobalStyles';
+import testContacts from '../../data/contacts.json';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -23,12 +24,7 @@ const notifyOptions = {
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: testContacts,
     filter: '',
   };
 
