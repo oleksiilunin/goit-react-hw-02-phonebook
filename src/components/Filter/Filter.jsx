3bf6067f330395ce0670e90e25dd85799bf18c1e
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-25 03:26:20 +0300

    Add propTypes and icon for label name

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { FiSearch } from 'react-icons/fi';
 
-const Filter = ({ state, onChangeInput }) => {
+import PropTypes from 'prop-types';
+
+const Filter = ({ filterValue, onChangeInput }) => {
   return (
     <label>
+      <FiSearch />
       Find contacts by name
       <input
         type="text"
         name="filter"
-        value={state.filter}
+        value={filterValue}
         pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         autoFocus
@@ -16,6 +20,11 @@ const Filter = ({ state, onChangeInput }) => {
       />
     </label>
   );
+};
+
+Filter.propTypes = {
+  filterValue: PropTypes.string.isRequired,
+  onChangeInput: PropTypes.func.isRequired,
 };
 
 export { Filter };
